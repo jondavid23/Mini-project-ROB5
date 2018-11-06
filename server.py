ac8195aea62b392fc35529cbbb2a1d4fd088f6c9
raggyftw <hansenoliie@gmail.com>
2018-11-06 09:27:52 +0100

    Sever changed to class
    Changed server to class and added lists to contain the rfid tags

--- server.py
+++ server.py
@@ -1,33 +1,59 @@
 #!python2
 import socket
 
-def Main():
-    host = '192.168.0.2'#'127.0.0.1'
-    port = 5000
+class TCPSever(object):
+    def __init__(self):
+        self.carrier_group_1 = []
+        self.carrier_group_2 = []
+        self.carrier_group_3 = []
+        self.first_run = True
 
-    s = socket.socket()
-    s.bind((host,port))
+    def connectToClient(self):
+        host = '192.168.0.2'#'127.0.0.1'
+        port = 5000
 
-    s.listen(1)
-    c, addr = s.accept()
-    print "Successfully established connection from: " + str(addr)
-    while True:
-        data = c.recv(1024)
-        if not data:
-            print "Failed to receive data from PLC."
-            break
-        print "Received RFID Tag from PlC is: " + str(data)
-        if (data == 1):
-            data = 1
-        elif(data == 2):
-            data = 2
-        elif(data == 3):
-            data = 3
-        else:
-            data = "No tag received"
-        print("Sending back: " + str(data))
-        c.send(data
-    c.close()
+        s = socket.socket()
+        s.bind((host,port))
+
+        s.listen(1)
+        c, addr = s.accept()
+        print "Successfully established connection from: " + str(addr)
+        while True:
+            data = c.recv(1024)
+            if not data:
+                print "Failed to receive data from PLC."
+                break
+            if (first_run == True):
+                if (data == 1):
+                    self.carrier_group_1.append(data)
+                    print("initialization status: Sucessfull")
+                elif(data == 2):
+                    self.carrier_group_2.append(data)
+                    print("initialization status: Sucessfull")
+                elif(data == 3):
+                    self.carrier_group_3.append(data)
+                    print("initialization status: Sucessfull")
+                else:
+                    data = 0
+                    print("initialization status: Failed..." )
+                    c.send(data
+                c.close()
+            else:
+                print "Received RFID Tag from PlC is: " + str(data)
+                if (data in self.carrier_group_1):
+                    data = 1
+                elif(data == data in self.carrier_group_2):
+                    data = 2
+                elif(data in self.carrier_group_3):
+                    data = 3
+                else:
+                    data = 0
+                print("Sending back: " + str(data))
+                c.send(data
+                c.close()
+
+
 
 if __name__ == '__main__':
-    Main()
+    server = TCPSever()
+    server.connectToClient()
