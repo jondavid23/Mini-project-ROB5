5b217ae7d24e88d54dd91d1ab3fc54adf1ee4a84
raggyftw <hansenoliie@gmail.com>
2018-11-06 08:53:45 +0100

    Modified sever
    modified process from PLC and send back instruction

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import socket
 
 def Main():
-    host = '172.20.40.46' #'127.0.0.1'
+    host = '192.168.0.2'#'127.0.0.1'
     port = 5000
 
     s = socket.socket()
@@ -10,15 +10,23 @@ def Main():
 
     s.listen(1)
     c, addr = s.accept()
-    print "Connection from: " + str(addr)
+    print "Successfully established connection from: " + str(addr)
     while True:
         data = c.recv(1024)
         if not data:
+            print "Failed to receive data from PLC."
             break
-        print "from connected user: " + str(data)
-        data = str(data).upper()
-        print "sending: " + str(data)
-        #c.send(data)
+        print "Received RFID Tag from PlC is: " + str(data)
+        if (data == 1):
+            data = 1
+        elif(data == 2):
+            data = 2
+        elif(data == 3):
+            data = 3
+        else:
+            data = "No tag received"
+        print("Sending back: " + str(data))
+        c.send(data
     c.close()
 
 if __name__ == '__main__':
